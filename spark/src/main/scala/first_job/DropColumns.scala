30b029df650dbea6265d39890a0373f74405d53b
Simone Venturi <simone.venturi5@studio.unibo.it>
2020-07-13 20:17:07 +0200

    Add utility task

--- spark/src/main/scala/first_job/DropColumns.scala
+++ spark/src/main/scala/first_job/DropColumns.scala
@@ -1,17 +1,30 @@
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 
+
+/**
+ * Application return a csv file without Primary Type and Description columns
+ *
+ */
 object DropColumns extends App {
-    val spark = SparkSession.builder.appName("BDE Spark Ventani Venturi").getOrCreate()
+  val spark = SparkSession.builder.appName("BDE Spark Ventani Venturi").getOrCreate()
+  val outputFile = "/user/sventuri/Crimes-2001-to-present-withoutDescription.csv"
+  val outputPath = new Path(outputFile)
+  val fileSystem = FileSystem.get(new Configuration())
+  if (fileSystem.exists(outputPath)) {
+    fileSystem.delete(outputPath, true)
+  }
     spark.read
       .format("csv")
       .option("sep", ";")
       .option("header", "true")
       .option("mode", "DROPMALFORMED")
-      .load("/user/sventuri/Crimes-2001-to-present.csv")
+      .load("/user/sventuri/Crimes-2001-to-present-nocomma.csv")
       .drop("Primary Type","Description")
-      .repartition(1)
+      .coalesce(1)
       .write.format("com.databricks.spark.csv")
       .option("sep", ";")
       .option("header", "true")
-      .save("/user/sventuri/Crimes-2001-to-present-withoutDescription.csv")
+      .save(outputFile)
 }
